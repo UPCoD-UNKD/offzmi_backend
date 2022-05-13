761ef8740d3b324baf8c1d3de3942615dd614c58
Dmitry <debrindima@gmail.com>
2022-05-13 15:10:21 +0300

    Adding id to user db func added

--- Create/__init__.py
+++ Create/__init__.py
@@ -5,14 +5,19 @@ from airtable import Airtable
 import random
 import bcrypt
 
-from requests import HTTPError
+import requests
 
 base_id = "appkzUYKXEL4LGVzf"
 table_name = 'offzmi_table'
 api_key = 'keyayJEttwX4dBeZv'
 
 airtable = Airtable(base_id, table_name, api_key)
-adalo_url = "https://api.adalo.com/v0/apps/b1610ffb-c5ee-4ec5-9a9e-3daae6af8061/collections/t_555b0678dcbf497fa89f55dbdd94fe21/{ElementId}"
+update_url = "https://api.adalo.com/v0/apps/b1610ffb-c5ee-4ec5-9a9e-3daae6af8061/collections/t_555b0678dcbf497fa89f55dbdd94fe21/"
+all_records_url = "https://api.adalo.com/v0/apps/b1610ffb-c5ee-4ec5-9a9e-3daae6af8061/collections/t_555b0678dcbf497fa89f55dbdd94fe21"
+
+adalo_headers = {
+    "Authorization": "Bearer duwa7prlbaojtqrc9cpiq5j9f"
+}
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -23,16 +28,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except ValueError:
         pass
     else:
-        try:
-            airtable.search('Email', req_body['Email'])
-        except HTTPError:
-            req_body['id'] = create_id()
-            req_body['salt'], req_body['Password'] = hash_password(req_body['Password'])
-            req_body['actionPoints'] = 0
-            response = airtable.insert(req_body)
-            return func.HttpResponse(f"Hello, {response}. This HTTP triggered function executed successfully.")
-        else:
-            return func.HttpResponse(f"User with this email already exists")
+        # try:
+        #     airtable.search('Email', req_body['Email'])
+        # except requests.HTTPError:
+        req_body['id'] = create_id()
+        req_body['salt'], req_body['Password'] = hash_password(req_body['Password'])
+        req_body['actionPoints'] = 0
+        response = airtable.insert(req_body)
+        records = requests.get(url=all_records_url, headers=adalo_headers)
+        requests.post(url=f"{update_url}{len(records.json()['records'])}", headers=adalo_headers, json={
+            'id': req_body['id']
+        })
+        return func.HttpResponse(f"Hello, {response}. This HTTP triggered function executed successfully.")
+        # else:
+        #     return func.HttpResponse(f"User with this email already exists")
 
 
 def create_id():
@@ -40,7 +49,7 @@ def create_id():
         uid = ''.join([str(random.randint(0, 999)).zfill(3) for _ in range(2)])
         try:
             airtable.search('ColumnB', uid)
-        except HTTPError:
+        except requests.HTTPError:
             return uid
 
 
