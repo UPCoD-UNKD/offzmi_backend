fe02a2006dcdfef3b4f4a20c52f0edc3ef9f89ab
Dmitry <debrindima@gmail.com>
2022-06-01 15:27:48 +0300

    email func updated

--- SendFeedback/__init__.py
+++ SendFeedback/__init__.py
@@ -7,11 +7,10 @@ from sendgrid.helpers.mail import Mail, Email, To, Content
 
 sg = SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
 from_email = Email("admin@offzmi.com")
-to_email = "admin@offzmi.com"
-subject = "Feedback від користувача!"
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+
     logging.info('Python HTTP trigger function processed a request.')
 
     try:
@@ -19,7 +18,31 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except ValueError:
         pass
     else:
-        content = Content('text/html', req_body['Feedback'])
+        if 'contactForm_email' in req_body:
+            to_email = req_body['contactForm_email']['value']
+
+            invitation_letter = """
+                Вітаємо!\n
+                \n
+                \n
+                Це Ваші персональні посилання для завантаження додатку:\n
+                https://play.google.com/store/apps/details?id=com.unkd.offzmi\n
+                https://apps.apple.com/us/app/off-zmi/id1626418738\n
+                \n
+                Будь ласка, перейдіть за посиланням відповідно до Вашої платформи, завантажте додаток та зареєструйтесь.\n
+                Ви можете розпочати публікацію новин одразу після авторизації.\n
+                \n
+                З найкращими побажаннями команда OffЗМІ!\n
+                \n
+                Нам важливо, що Ви з нами.
+            """
+            content = Content('text/html', invitation_letter)
+            subject = ''
+        else:
+            to_email = from_email
+            subject = "Feedback від користувача!"
+            content = Content('text/html', req_body['Feedback'])
+
         mail = Mail(from_email, To(to_email), subject, content)
         mail_json = mail.get()
         response = sg.client.mail.send.post(request_body=mail_json)
