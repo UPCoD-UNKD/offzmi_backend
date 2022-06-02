0cd8fd43badaf699dcfe600573a58ce42b2ca996
Dmitry <debrindima@gmail.com>
2022-06-02 13:23:15 +0300

    Update __init__.py

--- SendFeedback/__init__.py
+++ SendFeedback/__init__.py
@@ -19,35 +19,35 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         req_body = req.get_json()
         print(req_body)
         requests.post(url='https://eofbyoqrpojsf2.m.pipedream.net', data=req_body)
-    except ValueError:
-        pass
-    else:
-        if 'contactForm_email' in req_body['fields']:
-            to_email = req_body['fields']['contactForm_email']['value']
-
-            invitation_letter = """
-                Вітаємо!\n
-                \n
-                \n
-                Це Ваші персональні посилання для завантаження додатку:\n
-                https://play.google.com/store/apps/details?id=com.unkd.offzmi\n
-                https://apps.apple.com/us/app/off-zmi/id1626418738\n
-                \n
-                Будь ласка, перейдіть за посиланням відповідно до Вашої платформи, завантажте додаток та зареєструйтесь.\n
-                Ви можете розпочати публікацію новин одразу після авторизації.\n
-                \n
-                З найкращими побажаннями команда OffЗМІ!\n
-                \n
-                Нам важливо, що Ви з нами.
-            """
-            content = Content('text/html', invitation_letter)
-            subject = 'Запрошення до OffЗМІ'
-        else:
-            to_email = from_email
-            subject = "Feedback від користувача!"
-            content = Content('text/html', req_body['Feedback'])
-
-        mail = Mail(from_email, To(to_email), subject, content)
-        mail_json = mail.get()
-        response = sg.client.mail.send.post(request_body=mail_json)
-        return func.HttpResponse(f'{response.status_code}, {response.headers}')
+    # except ValueError:
+    #     pass
+    # else:
+    #     if 'contactForm_email' in req_body['fields']:
+    #         to_email = req_body['fields']['contactForm_email']['value']
+    #
+    #         invitation_letter = """
+    #             Вітаємо!\n
+    #             \n
+    #             \n
+    #             Це Ваші персональні посилання для завантаження додатку:\n
+    #             https://play.google.com/store/apps/details?id=com.unkd.offzmi\n
+    #             https://apps.apple.com/us/app/off-zmi/id1626418738\n
+    #             \n
+    #             Будь ласка, перейдіть за посиланням відповідно до Вашої платформи, завантажте додаток та зареєструйтесь.\n
+    #             Ви можете розпочати публікацію новин одразу після авторизації.\n
+    #             \n
+    #             З найкращими побажаннями команда OffЗМІ!\n
+    #             \n
+    #             Нам важливо, що Ви з нами.
+    #         """
+    #         content = Content('text/html', invitation_letter)
+    #         subject = 'Запрошення до OffЗМІ'
+    #     else:
+    #         to_email = from_email
+    #         subject = "Feedback від користувача!"
+    #         content = Content('text/html', req_body['Feedback'])
+    #
+    #     mail = Mail(from_email, To(to_email), subject, content)
+    #     mail_json = mail.get()
+    #     response = sg.client.mail.send.post(request_body=mail_json)
+    #     return func.HttpResponse(f'{response.status_code}, {response.headers}')
