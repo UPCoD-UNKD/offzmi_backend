0041d6fc2e5fcc45b4cd6c2b912d8662b5bf512b
Dmitry <debrindima@gmail.com>
2022-06-02 13:11:41 +0300

    Update __init__.py

--- SendFeedback/__init__.py
+++ SendFeedback/__init__.py
@@ -17,7 +17,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
         req_body = req.get_json()
-        requests.post(url='https://eofbyoqrpojsf2.m.pipedream.net', body=req_body)
+        requests.post(url='https://eofbyoqrpojsf2.m.pipedream.net', data=req_body)
     except ValueError:
         pass
     else:
